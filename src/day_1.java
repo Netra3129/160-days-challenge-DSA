97993508cef3e78648dccf8bbebf97395fdd0d29
Netranjana <ghoshnetranjana@gmail.com>
2024-12-03 19:34:24 +0530

    second LArgest element in an array

--- src/day_1.java
+++ src/day_1.java
@@ -1,6 +1,30 @@
 public class day_1 {
+        public static int getSecondLargest(int[] arr) {
+            int n=arr.length;
+            int large=-1;
+            int secondLarge=-1;
+            int i;
+            for(i=0;i<n;i++){
+                if(arr[i]>large){
+                    secondLarge=large;
+                    large=arr[i];
+                }
+                else if(arr[i]>secondLarge && arr[i]!=large){
+                    secondLarge=arr[i];
+                }
+                //else if(arr[i]>secondLarge && arr[i]!=large){
+                //secondLarge=arr[i];
+
+                //}
+            }
+            return secondLarge;
+
+        }
+
     public static void main(String[] args) {
-        System.out.println("HEllo World");
-        System.out.println("netranjana");
+        int[] zrr= {25,34,17,18,37};
+        int secondL=getSecondLargest(zrr);
+        System.out.println(secondL);
     }
-}
+    }
+
